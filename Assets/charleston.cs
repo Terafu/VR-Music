0e020703efe4871d10e056fecd032484ca560f2c
Terafu <maxime-dufour@outlook.fr>
2017-12-21 13:08:02 +0100

    Add project to git

---
+++ Assets/charleston.cs
@@ -0,0 +1,39 @@
+﻿using HTC.UnityPlugin.ColliderEvent;
+using HTC.UnityPlugin.Utility;
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class charleston : MonoBehaviour
+    , IColliderEventHoverEnterHandler
+{
+
+    AudioSource audio;
+    bool playMusic = true;
+    tambours script;
+
+    private void Start()
+    {
+        script = (tambours) GameObject.Find("tambours").GetComponent("tambours");
+        audio = GetComponent<AudioSource>();
+        audio.Stop();
+
+    }
+
+    public void OnColliderEventHoverEnter(ColliderHoverEventData eventData)
+    {
+        Debug.Log(8 - script.currentTime % 8);
+
+        if (playMusic)
+        {
+            playMusic = false;
+            audio.Stop();
+        }
+
+        else
+        {
+            playMusic = true;
+            audio.PlayDelayed(8 - script.currentTime % 8);
+        }
+    }
+}
---
+++ Assets/tambours.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tambours : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		
+	}
+
+    public float currentTime = 0.0f;
+
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        //Debug.Log(AudioSettings.dspTime);
+    }
+}
